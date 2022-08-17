26a214e232ae55f74fae3fea8267beb3670e2a03
DmitrySuprun <suprun1212@gmail.com>
2022-08-17 13:30:27 +0300

    Start project

---
+++ RoadmapTestAssignment/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RoadmapTestAssignment
+//
+//  Created by Дмитрий Супрун on 16.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+}
+
